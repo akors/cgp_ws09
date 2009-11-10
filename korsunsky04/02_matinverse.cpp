92dccfb8624448570cd2dd70b564d27922c0c13e
Alexander Korsunsky <fat.lobyte9@gmail.com>
2009-11-10 22:21:25 +0100

    Matrixmultiplikation und Probe hinzugefügt

--- korsunsky04/02_matinverse.cpp
+++ korsunsky04/02_matinverse.cpp
@@ -9,24 +9,20 @@ typedef std::valarray<double> matrix_t;
 
 const unsigned n = 2;
 
-matrix_t read2x2();
-void print2x2(const matrix_t& m);
+
 
 // own comparison function, because comparing for equality doesnt work
 // reliably
-bool is_equal(const double& a, const double& b)
-{
-    return std::fabs(a - b) < std::numeric_limits<double>::epsilon();
-}
+inline bool is_equal(const double& a, const double& b)
+{ return std::fabs(a - b) < std::numeric_limits<double>::epsilon(); }
 
+inline matrix_t::value_type determinant2x2(const matrix_t& m)
+{ return m[0]*m[3] - m[1]*m[2]; }
 
-inline
-matrix_t::value_type determinant2x2(const matrix_t& m)
-{
-    return m[0]*m[3] - m[1]*m[2];
-}
-
+matrix_t m_multiply2x2(const matrix_t& A, const matrix_t& B);
 matrix_t invert2x2(const matrix_t& m);
+matrix_t read2x2();
+void print2x2(const matrix_t& m);
 
 int main()
 {
@@ -44,8 +40,12 @@ int main()
             return 1;
         }
 
+        matrix_t inv = invert2x2(mat);
         std::cout<<"Invertierte Matrix A^-1 = \n";
-        print2x2(invert2x2(mat));
+        print2x2(inv);
+
+        std::cout<<"Probe: A * A^-1 = \n";
+        print2x2(m_multiply2x2(mat, inv));
     } catch(...)
     {
         std::cerr<<"Fehler beim Einlesen der Matrix\n";
@@ -97,5 +97,14 @@ void print2x2(const matrix_t& m)
     std::cout<<"["<<m[2]<<", "<<m[3]<<"]\n";
 }
 
+matrix_t m_multiply2x2(const matrix_t& A, const matrix_t& B)
+{
+    matrix_t result(n*n);
 
+    result[0] = A[0]*B[0] + A[1]*B[2];
+    result[1] = A[0]*B[1] + A[1]*B[3];
+    result[2] = A[2]*B[0] + A[3]*B[2];
+    result[3] = A[2]*B[1] + A[3]*B[3];
 
+    return result;
+}
