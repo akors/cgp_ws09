4bc577322b16674dc6b943450ce9ded0d6329a2a
Alexander Korsunsky <fat.lobyte9@gmail.com>
2009-11-10 22:07:00 +0100

    Invertieren der Matrix implementiert

--- korsunsky04/02_matinverse.cpp
+++ korsunsky04/02_matinverse.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <valarray>
+#include <cmath>
+#include <limits>
 
 using std::slice;
 
 typedef std::valarray<double> matrix_t;
+
 const unsigned n = 2;
 
 matrix_t read2x2();
 void print2x2(const matrix_t& m);
+
+// own comparison function, because comparing for equality doesnt work
+// reliably
+bool is_equal(const double& a, const double& b)
+{
+    return std::fabs(a - b) < std::numeric_limits<double>::epsilon();
+}
+
 
 inline
 matrix_t::value_type determinant2x2(const matrix_t& m)
@@ -15,14 +26,26 @@ matrix_t::value_type determinant2x2(const matrix_t& m)
     return m[0]*m[3] - m[1]*m[2];
 }
 
-
+matrix_t invert2x2(const matrix_t& m);
 
 int main()
 {
     try {
         matrix_t mat = read2x2();
 
-        print2x2(mat);
+        std::cout<<"A = \n"; print2x2(mat); std::cout<<"\n\n";
+
+        std::cout<<"det(A) = "<<determinant2x2(mat)<<'\n';
+
+        if(is_equal(determinant2x2(mat),0))
+        {
+            std::cout<<"Die Determinante ist 0. Kann Inverse Matrix nicht "
+                "berechnen\n";
+            return 1;
+        }
+
+        std::cout<<"Invertierte Matrix A^-1 = \n";
+        print2x2(invert2x2(mat));
     } catch(...)
     {
         std::cerr<<"Fehler beim Einlesen der Matrix\n";
@@ -44,10 +67,28 @@ matrix_t read2x2()
 
     std::cout<<"Geben sie die "<<n<<"x"<<n<<" Matrix Zeilenweise ein (trennen"
         "der Elemente durch Leerzeichen):\n";
-    std::cout<<"["; std::cin>>mat[0]; std::cin>>mat[1]; std::cout<<"]\n";
-    std::cout<<"["; std::cin>>mat[2]; std::cin>>mat[3]; std::cout<<"]\n";
+    std::cout<<"[\n\t";
+        std::cin>>mat[0]; std::cin>>mat[1];
+    std::cout<<"]\n";
+    std::cout<<"[\n\t";
+        std::cin>>mat[2]; std::cin>>mat[3];
+    std::cout<<"]\n";
 
     return mat;
+}
+
+matrix_t invert2x2(const matrix_t& m)
+{
+    matrix_t inverted(n*n);
+
+    inverted[0] = m[3];
+    inverted[1] = -m[1];
+    inverted[2] = -m[2];
+    inverted[3] = m[0];
+
+    inverted *= (1.0/determinant2x2(m));
+
+    return inverted;
 }
 
 void print2x2(const matrix_t& m)
